814fa1b245a3ddd44ba9e0b011d07bf63524da0d
QianT <1816107581@qq.com>
2023-09-22 19:39:54 +0800

    项目框架搭建：feat:init DDD project

---
+++ chat-api-common/src/main/java/cn/tina/ai/Main.java
@@ -0,0 +1,12 @@
+package cn.tina.ai;
+
+
+/**
+ * @author TQ
+ * @since 2023-09-22 17:59
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ chat-api-interfaces/src/main/java/cn/tina/ai/ApiApplication.java
@@ -0,0 +1,18 @@
+package cn.tina.ai;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author TQ
+ * @description 启动入口
+ * @github <a href="https://github.com/QianTina">...</a>
+ * @since 2023-09-22 17:54
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
